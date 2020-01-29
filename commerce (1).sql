efded137abeb9b04ae9268beaac6013933387ce5
yvanroan <42220010+yvanroan@users.noreply.github.com>
2020-01-29 15:01:59 -0600

    Add files via upload

---
+++ commerce (1).sql
@@ -0,0 +1,167 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  mer. 29 jan. 2020 à 21:00
+-- Version du serveur :  5.7.21
+-- Version de PHP :  5.6.35
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `commerce`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cart`
+--
+
+DROP TABLE IF EXISTS `cart`;
+CREATE TABLE IF NOT EXISTS `cart` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `productId` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `ownerid` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ownerid` (`ownerid`),
+  KEY `productId` (`productId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE IF NOT EXISTS `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(500) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `image` varchar(500) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `price` float NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `productentity`
+--
+
+DROP TABLE IF EXISTS `productentity`;
+CREATE TABLE IF NOT EXISTS `productentity` (
+  `productid` int(11) NOT NULL,
+  `cartid` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  KEY `productid` (`productid`),
+  KEY `cartid` (`cartid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `receipt`
+--
+
+DROP TABLE IF EXISTS `receipt`;
+CREATE TABLE IF NOT EXISTS `receipt` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` varchar(15) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `total` int(11) NOT NULL,
+  `ownerid` int(11) NOT NULL,
+  `cartid` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `cartid` (`cartid`),
+  KEY `FK_userreceipt` (`ownerid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `review`
+--
+
+DROP TABLE IF EXISTS `review`;
+CREATE TABLE IF NOT EXISTS `review` (
+  `productId` int(11) NOT NULL,
+  `consumerId` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `stars` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  KEY `consumerId` (`consumerId`),
+  KEY `FK_productreview` (`productId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `name` varchar(15) NOT NULL,
+  `number` varchar(14) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `mail` varchar(30) NOT NULL,
+  `addresse` varchar(200) NOT NULL,
+  `profile` varchar(200) NOT NULL,
+  `age` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `FK_cartuser` FOREIGN KEY (`ownerid`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`productId`) REFERENCES `product` (`id`);
+
+--
+-- Contraintes pour la table `productentity`
+--
+ALTER TABLE `productentity`
+  ADD CONSTRAINT `productentity_ibfk_1` FOREIGN KEY (`productid`) REFERENCES `product` (`id`),
+  ADD CONSTRAINT `productentity_ibfk_2` FOREIGN KEY (`cartid`) REFERENCES `cart` (`id`);
+
+--
+-- Contraintes pour la table `receipt`
+--
+ALTER TABLE `receipt`
+  ADD CONSTRAINT `FK_receiptcart` FOREIGN KEY (`cartid`) REFERENCES `cart` (`id`),
+  ADD CONSTRAINT `FK_userreceipt` FOREIGN KEY (`ownerid`) REFERENCES `user` (`id`);
+
+--
+-- Contraintes pour la table `review`
+--
+ALTER TABLE `review`
+  ADD CONSTRAINT `FK_productreview` FOREIGN KEY (`productId`) REFERENCES `product` (`id`),
+  ADD CONSTRAINT `review_ibfk_1` FOREIGN KEY (`consumerId`) REFERENCES `user` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
