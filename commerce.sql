05d3c94c2e7c61e41b3f12adc4e24ca132500751
Arnol Fokam <arnolfokam23@gmail.com>
2020-01-29 20:23:48 +0100

    deleted supplier table

--- commerce.sql
+++ commerce.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.6.6deb5
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  mer. 29 jan. 2020 à 18:54
--- Version du serveur :  5.7.21
--- Version de PHP :  5.6.35
+-- Host: localhost:3306
+-- Generation Time: Jan 29, 2020 at 08:22 PM
+-- Server version: 5.7.29-0ubuntu0.18.04.1
+-- PHP Version: 7.2.24-0ubuntu0.18.04.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,138 +17,173 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `commerce`
+-- Database: `ecommerce`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `cart`
+-- Table structure for table `cart`
 --
 
-DROP TABLE IF EXISTS `cart`;
-CREATE TABLE IF NOT EXISTS `cart` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL,
   `productId` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
-  `ownerid` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `ownerid` (`ownerid`),
-  KEY `productId` (`productId`)
+  `ownerid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `product`
+-- Table structure for table `product`
 --
 
-DROP TABLE IF EXISTS `product`;
-CREATE TABLE IF NOT EXISTS `product` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
   `description` varchar(500) NOT NULL,
   `name` varchar(150) NOT NULL,
   `image` varchar(500) NOT NULL,
   `quantity` int(11) NOT NULL,
-  `price` float NOT NULL,
-  PRIMARY KEY (`id`)
+  `price` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `receipt`
+-- Table structure for table `receipt`
 --
 
-DROP TABLE IF EXISTS `receipt`;
-CREATE TABLE IF NOT EXISTS `receipt` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `receipt` (
+  `id` int(11) NOT NULL,
   `date` varchar(15) NOT NULL,
   `description` varchar(200) NOT NULL,
   `total` int(11) NOT NULL,
-  `ownerid` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_userreceipt` (`ownerid`)
+  `ownerid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `review`
+-- Table structure for table `review`
 --
 
-DROP TABLE IF EXISTS `review`;
-CREATE TABLE IF NOT EXISTS `review` (
+CREATE TABLE `review` (
   `productId` int(11) NOT NULL,
   `consumerId` int(11) NOT NULL,
   `description` text NOT NULL,
   `stars` int(11) NOT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`),
-  KEY `consumerId` (`consumerId`),
-  KEY `FK_productreview` (`productId`)
+  `id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `supplier`
+-- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `supplier`;
-CREATE TABLE IF NOT EXISTS `supplier` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `number` varchar(14) NOT NULL,
-  `name` varchar(15) NOT NULL,
-  `address` varchar(200) NOT NULL,
-  `description` varchar(300) NOT NULL,
-  `mail` varchar(30) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE `user` (
   `name` varchar(15) NOT NULL,
   `number` varchar(14) NOT NULL,
   `password` varchar(20) NOT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `mail` varchar(30) NOT NULL,
   `addresse` varchar(200) NOT NULL,
   `profile` varchar(200) NOT NULL,
-  `age` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  `age` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Contraintes pour les tables déchargées
+-- Indexes for dumped tables
 --
 
 --
--- Contraintes pour la table `cart`
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `ownerid` (`ownerid`),
+  ADD KEY `productId` (`productId`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `receipt`
+--
+ALTER TABLE `receipt`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_userreceipt` (`ownerid`);
+
+--
+-- Indexes for table `review`
+--
+ALTER TABLE `review`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `consumerId` (`consumerId`),
+  ADD KEY `FK_productreview` (`productId`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `receipt`
+--
+ALTER TABLE `receipt`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `review`
+--
+ALTER TABLE `review`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cart`
 --
 ALTER TABLE `cart`
   ADD CONSTRAINT `FK_cartuser` FOREIGN KEY (`ownerid`) REFERENCES `user` (`id`),
   ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`productId`) REFERENCES `product` (`id`);
 
 --
--- Contraintes pour la table `receipt`
+-- Constraints for table `receipt`
 --
 ALTER TABLE `receipt`
   ADD CONSTRAINT `FK_userreceipt` FOREIGN KEY (`ownerid`) REFERENCES `user` (`id`);
 
 --
--- Contraintes pour la table `review`
+-- Constraints for table `review`
 --
 ALTER TABLE `review`
   ADD CONSTRAINT `FK_productreview` FOREIGN KEY (`productId`) REFERENCES `product` (`id`),
   ADD CONSTRAINT `review_ibfk_1` FOREIGN KEY (`consumerId`) REFERENCES `user` (`id`);
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
